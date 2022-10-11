43f34f0c027f3af08f893afc4d4eacbfb5e7019a
Annisa Nuri Nabila <hello.annisann@gmail.com>
2022-10-11 16:53:01 +0700

    init assignments

---
+++ assignments/biodata.go
@@ -0,0 +1,22 @@
+package main
+
+import "os"
+
+/*
+	INSTRUCTIONS:
+		Create a CLI service to presend friends data in class using `struct` and `function`.
+
+		Data:
+			1. Name
+			2. Address
+			3. Job
+			4. Reasons why choose Golang
+
+
+		Expected output: data with student ID = 1
+		P.S. To get argument from terminal, use `os.Args`
+*/
+
+func main() {
+	inp := os.Args
+}
