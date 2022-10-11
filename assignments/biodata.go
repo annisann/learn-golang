e15e12cf12ba9cd6840f4aa3c07ad8988ff2fb31
Annisa Nuri Nabila <hello.annisann@gmail.com>
2022-10-11 20:48:56 +0700

    assignment one

--- assignments/biodata.go
+++ assignments/biodata.go
@@ -1,6 +1,9 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 /*
 	INSTRUCTIONS:
@@ -17,6 +20,29 @@ import "os"
 		P.S. To get argument from terminal, use `os.Args`
 */
 
+type Student struct {
+	ID      int
+	Name    string
+	Address string
+	Job     string
+	Reasons string
+}
+
+var students = []Student{}
+
+func addStudents(args []string) {
+	var newStudent = Student{
+		ID:      len(students) + 1,
+		Name:    args[1],
+		Address: args[2],
+		Job:     args[3],
+		Reasons: args[4],
+	}
+	students = append(students, newStudent)
+}
+
 func main() {
 	inp := os.Args
+	addStudents(inp)
+	fmt.Println(students)
 }
