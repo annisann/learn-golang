b4fb83e56b6227df5be2f85beace5361f82ae5a4
Annisa Nuri Nabila <ennoza@student.ub.ac.id>
2022-10-12 22:27:19 +0700

    add createUser and getUserById functions

--- 07_gorm/main.go
+++ 07_gorm/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "07_gorm/database"
+import (
+	"07_gorm/database"
+	"07_gorm/models"
+	"errors"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+func createUser(email string) {
+	db := database.GetDB()
+
+	User := models.User{
+		Email: email,
+	}
+
+	// create data
+	err := db.Create(&User).Error
+	if err != nil {
+		fmt.Println("Error creating user data: ", err)
+		return
+	}
+
+	fmt.Println("New user data: ", User)
+}
+
+func getUserById(id uint) {
+	db := database.GetDB()
+
+	user := models.User{}
+
+	// get the first data
+	err := db.First(&user, "id=?", id).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			fmt.Println("User data not found")
+			return
+		}
+		print("Error finding user", err)
+	}
+	fmt.Printf("User data: %+v \n", user)
+}
 
 func main() {
 	database.StartDB()
+
+	// createUser("hello@doe.com")
+	getUserById(1)
 }
