d509971491147d791a704876f80febefc72f4b6f
Annisa Nuri Nabila <ennoza@student.ub.ac.id>
2022-10-14 01:15:33 +0700

    add functions

--- 09_http_request/http_basic_auth/student.go
+++ 09_http_request/http_basic_auth/student.go
@@ -7,3 +7,24 @@ type Student struct {
 	Name  string
 	Grade int32
 }
+
+func GetStudents() []*Student {
+	return students
+}
+
+func SelectStudent(id string) *Student {
+	for _, each := range students {
+		if each.ID == id {
+			return each
+		}
+	}
+	return nil
+}
+
+func init() {
+	students = append(students, &Student{ID: "S001", Name: "Haley Dunphy", Grade: 6})
+	students = append(students, &Student{ID: "S002", Name: "Manny Delgado", Grade: 3})
+	students = append(students, &Student{ID: "S003", Name: "Alexander Dunphy", Grade: 4})
+	students = append(students, &Student{ID: "S004", Name: "Lily Tucker-Pritchett", Grade: 1})
+	students = append(students, &Student{ID: "S005", Name: "Luke Dunphy", Grade: 3})
+}
