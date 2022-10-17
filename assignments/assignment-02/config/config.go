c6e64b62d3618f774e735dc244a79939f1b95457
Annisa Nuri Nabila <ennoza@student.ub.ac.id>
2022-10-18 03:52:26 +0700

    config database

--- assignments/assignment-02/config/config.go
+++ assignments/assignment-02/config/config.go
@@ -1,1 +1,29 @@
 package config
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	host     = "localhost"
+	user     = "annisann"
+	password = "annisann"
+	dbport   = "1234"
+	dbname   = "order_by"
+)
+
+func InitDB() *gorm.DB {
+	// config to connect database
+	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+		host, user, password, dbname, dbport)
+
+	db, err := gorm.Open(postgres.Open(config), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err)
+	}
+	return db
+}
